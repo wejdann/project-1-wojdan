b8f6b0106132d9b03b38d807200e9cc4db7e9033
wejdann <weejdansaud58@hotmail.com>
2020-02-05 13:27:41 +0300

    new feture

--- main.js
+++ main.js
@@ -3,91 +3,88 @@ console.log('Hi');
 let boxes = document.querySelectorAll('.boxes');
 //switch btw player by let X=1 and O=0
 let player = 1;
+let turnsCount = 0;
+
 function switchPlayer() {
+    turnsCount++;
     if (player == 1) {
         this.innerHTML = "X";
+        document.getElementById("move").textContent = "Turn To Player 'O'";
         player = 0;
     } else {
         this.innerHTML = "O";
+        document.getElementById("move").textContent = "Turn To Player 'X'";
         player = 1;
     }
     //after player chose the box can not change it 
-    this.removeEventListener("click",switchPlayer );
+    this.removeEventListener("click", switchPlayer);
     //call function to check condition 
     checkForWins();
 }
 // for loop for boxes
 for (let i = 0; i < boxes.length; i++) {
-    boxes[i].addEventListener("click", switchPlayer); 
+    boxes[i].addEventListener("click", switchPlayer);
 }
 //get all boxes by ID
-var Box0 = document.getElementById("Box0");
-var Box1 = document.getElementById("Box1");
-var Box2 = document.getElementById("Box2");
-var Box5 = document.getElementById("Box5");
-var Box4 = document.getElementById("Box4");
-var Box3 = document.getElementById("Box3");
-var Box6 = document.getElementById("Box6");
-var Box7 = document.getElementById("Box7");
-var Box8 = document.getElementById("Box8");
-function stopAftwer(){
+var allBoxes = document.getElementById("Box0", "Box1", "Box2", "Box3", "Box4", "Box5", "Box6", "Box7", "Box8");
+//function to stop game after win
+function stopGameAfterWin() {
     for (let i = 0; i < boxes.length; i++) {
-        boxes[i].removeEventListener("click", switchPlayer); 
+        boxes[i].removeEventListener("click", switchPlayer);
     }
 }
-
-
 //function to check the boxes of winer 
 function checkForWins() {
-    if (Box0.innerHTML!=="" && Box0.innerHTML === Box1.innerHTML && Box0.innerHTML === Box2.innerHTML) {
+    //this if check boxes in row(1) >>[0],[1],[2] , then return massage and  stop game by call function
+    if (Box0.innerHTML !== "" && Box0.innerHTML === Box1.innerHTML && Box0.innerHTML === Box2.innerHTML) {
+       //var colorBoxwin= document.getElementById("Box0","Box1","Box2").style.backgroundColor='blue';
         document.getElementById("Win").textContent = "congrats you Win";
-        stopAftwer();
-    }    
-    else if (Box3.innerHTML!=="" && Box3.innerHTML === Box4.innerHTML && Box3.innerHTML === Box5.innerHTML){
-
-        document.getElementById("Win").textContent = "congrats you Win"; 
-        stopAftwer();}
-
-    else if (Box7.innerHTML!=="" && Box7.innerHTML === Box7.innerHTML && Box6.innerHTML === Box8.innerHTML){
-
-        adocument.getElementById("Win").textContent = "congrats you Win";
-        stopAftwer();}
-
-    else if (Box0.innerHTML!=="" && Box0.innerHTML === Box3.innerHTML && Box0.innerHTML === Box6.innerHTML){
-
+        stopGameAfterWin();
+    }
+    //this if check boxes in row(2) >>[3],[4],[5] , ~~~~~
+    else if (Box3.innerHTML !== "" && Box3.innerHTML === Box4.innerHTML && Box3.innerHTML === Box5.innerHTML) {
         document.getElementById("Win").textContent = "congrats you Win";
-        stopAftwer();}
-
-    else if ( Box1.innerHTML!=="" && Box1.innerHTML === Box4.innerHTML && Box1.innerHTML === Box7.innerHTML){
-
+        stopGameAfterWin();
+    }
+    //this if check boxes in row(3) >>[6],[7],[8] , ~~~~~
+    else if (Box6.innerHTML !== "" && Box6.innerHTML === Box7.innerHTML && Box6.innerHTML === Box8.innerHTML) {
         document.getElementById("Win").textContent = "congrats you Win";
-        stopAftwer();}
-
-    else if (Box2.innerHTML!=="" && Box2.innerHTML === Box5.innerHTML && Box2.innerHTML === Box8.innerHTML){
-
+        stopGameAfterWin();
+    }
+    //this if check boxes in colum(1) >>[0],[3],[6] , ~~~~~
+    else if (Box0.innerHTML !== "" && Box0.innerHTML === Box3.innerHTML && Box0.innerHTML === Box6.innerHTML) {
         document.getElementById("Win").textContent = "congrats you Win";
-        stopAftwer();}
-
-    else if (Box0.innerHTML!=="" && Box0.innerHTML === Box4.innerHTML && Box0.innerHTML === Box8.innerHTML){
-
+        stopGameAfterWin();
+    }
+    //this if check boxes in colum(2) >>[1],[4],[5] , ~~~~~
+    else if (Box1.innerHTML !== "" && Box1.innerHTML === Box4.innerHTML && Box1.innerHTML === Box7.innerHTML) {
         document.getElementById("Win").textContent = "congrats you Win";
-        stopAftwer();}
-
-    else if ( Box2.innerHTML!=="" &&  Box2.innerHTML === Box4.innerHTML && Box2.innerHTML === Box6.innerHTML){
-
+        stopGameAfterWin();
+    }
+    //this if check boxes in colum(3) >>[2],[5],[8] , ~~~~~
+    else if (Box2.innerHTML !== "" && Box2.innerHTML === Box5.innerHTML && Box2.innerHTML === Box8.innerHTML) {
         document.getElementById("Win").textContent = "congrats you Win";
-        stopAftwer();}
-    else {
-        document.getElementById("Win").textContent = "The game tie";
+        stopGameAfterWin();
+    }
+    //this if check boxes in cross (left-side) >>[0],[4],[8] , ~~~~~
+    else if (Box0.innerHTML !== "" && Box0.innerHTML === Box4.innerHTML && Box0.innerHTML === Box8.innerHTML) {
+        document.getElementById("Win").textContent = "congrats you Win";
+        stopGameAfterWin();
+    }
+    //this if check boxes in cross (right-side) >>[2],[4],[6] , ~~~~~
+    else if (Box2.innerHTML !== "" && Box2.innerHTML === Box4.innerHTML && Box2.innerHTML === Box6.innerHTML) {
+        document.getElementById("Win").textContent = "congrats you Win";
+        stopGameAfterWin();
+    } else if (turnsCount == 9) {
+        document.getElementById("Win").textContent = "The game is tie !";
     }
 }
-
-
+//this function for clear boxes and allow to play agine when click the button
 function replay() {
-
     for (let i = 0; i < boxes.length; i++) {
-        boxes[i].addEventListener("click", switchPlayer); 
-        boxes[i].innerHTML="";
+        boxes[i].addEventListener("click", switchPlayer);
+        boxes[i].innerHTML = "";
+        turnsCount = 0;
     }
     player = 1;
 }
